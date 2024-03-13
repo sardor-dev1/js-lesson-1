2a7e4ca179d9bb2d71684bc4d16688f9ffcb7c38
sardor-dev1 <sardorprogramist@gmail.com>
2024-03-13 21:39:29 +0500

    added examples

---
+++ app.js
@@ -0,0 +1,46 @@
+//begin-1
+// let a = +prompt("kvadratning tomonini kiring");
+// let p = 4 * a;
+// console.log(p);
+
+//begin-2
+// let a = +prompt("kvadratning tomonini kiring");
+// let y = a ** 2;
+// console.log(y);
+
+// begin-3
+// let a = +prompt("to'rtburchakning a tomonini kiriting");
+// let b = +prompt("to'rtburchakning b tomonini kiriting");
+// let S = a * b;
+// let P = 2 * (a + b);
+// console.log(`${S} yuza`);
+// console.log(`${P} perimetr`);
+
+//begin-4
+// let d = +prompt("diametr kiring");
+// const pi = 3.14;
+// const L = d * pi;
+// console.log(`${L} uzunlik`);
+
+// begin-5
+let a = +prompt("Kubning tomonini kiriting");
+let V = a ** 3;
+let S = 6 * a ** 2;
+
+console.log(`${V} kubning hajmi`);
+console.log(`${S} to'la sirti`);
+
+//begin-22
+// let A = +prompt("A ning qiymatini kiriting:");
+// let B = +prompt("B ning qiymatini kiriting:");
+// let C = +prompt("C ning qiymatini kiriting:");
+
+// console.log("Boshlang'ich:");
+// console.log(A);
+// console.log(B);
+// console.log(C);
+
+// console.log("Almashtirilgan");
+// console.log(A);
+// console.log(B);
+// console.log(C);
